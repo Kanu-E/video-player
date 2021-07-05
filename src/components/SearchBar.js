36b930cef8563928797ff52f53b39efd395c0486
kanu-e <chukwuemekakanu@gmail.com>
2021-07-05 17:12:24 -0500

    add search form to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
+
+        state = {searchTerm: ''}
+
+        onHandleChange=()=>{
+            console.log('changed')
+        }
         render(){
+  
             return(
-                <div>
-                    search bar
+                <div className= "search-bar ui segment">
+                    <form className= "ui form">
+                       <div className= "field">
+                       <label>Video Search</label>
+                        <input type='text' value={this.state.searchTerm} 
+                        onChange={this.onHandleChange}/>
+                       </div>
+                        <input type='submit'/>
+                    </form>
                 </div>
             )
         }
