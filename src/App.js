aace5443c52b0afa8585089e9e20c42fa5896773
kanu-e <chukwuemekakanu@gmail.com>
2021-07-07 14:31:21 -0500

    create videolist and pass videos from App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SearchBar from './components/SearchBar'
 import youtube from './apis/youtube'
+import VideoList from './components/VideoList'
 
 class App extends React.Component {
 
-  sta
+  state = {videoList: []}
 
   onSearchSubmit = async term =>{
     const response = await youtube.get('/search',{
@@ -12,14 +13,14 @@ class App extends React.Component {
         q: term
       }
     });
-    this.setState({searchTerm: response.data.items})
+    this.setState({videoList: response.data.items})
   }
 
   render(){
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onSearchSubmit}/>
-        I have {this.state.searchTerm.length} Videos
+        <VideoList videos= {this.state.videoList}/>
       </div>
     );
   }
